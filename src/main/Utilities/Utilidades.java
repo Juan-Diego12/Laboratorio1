663bb2fb03a4f0e1b856b63201831f7012dc4140
DanielJNarvaezH <danielj.narvaezh@uqvirtual.edu.co>
2024-09-22 21:25:36 -0500

    Update Utilidades.java

--- src/main/Utilities/Utilidades.java
+++ src/main/Utilities/Utilidades.java
@@ -1,3 +1,45 @@
 public class Utilidades {
 
+    //Serializar un objeto en archivo binario
+    public static void serializarObjeto(String nombre, Object objeto) throws IOException{
+        ObjetoOutputStream salida;
+
+        salida = new  ObjectOutputStream(new FileOutputStream(nombre));
+        salida.writeObject(objeto);
+        salida.close();
+    }
+
+    //Deserializar un objeto en un archivo binario
+    public static Object deserializarObjeto( String nombre) throws Exception{
+        Object objeto;
+        ObjetoInputStream entrada;
+
+        entrada = new ObjetoInputStream( new FileInputStream(nombre));
+        objeto = entrada.readObject();
+        entrada.close();
+
+        return objeto;
+    }
+
+    //Serializar un objeto en un archivo de XML
+    public static void serializarObjetoXML(String nombre, Object objeto) throws IOException{
+
+        XMLEncoder codificador;
+
+        codificador = new XMLEncoder( new FileOutputStream(nombre));
+        codificador.writeObject(objeto);
+        codificador.close();
+    }
+
+    //Deserializar un objeto en un archivo  XML
+    public static Object deserializarObjetoXML (String nombre) throws IOException{
+        XMLDecoder decodificador;
+        Object objeto;
+
+        decodificador = new XMLDecorder( new FileInputStream(nombre));
+        objeto = decodificador.readObject();
+        decodificador.close();
+
+        return objeto;
+    }
 }
